7c7eaf6fe1e30eadc23a9bc1606432a2d73fb394
ZhuoyunZhong <zzhong3@wpi.edu>
2021-07-15 00:31:22 -0400

    add new models

--- Assets/Scripts/SimpleController.cs
+++ Assets/Scripts/SimpleController.cs
@@ -4,35 +4,33 @@ using UnityEngine;
 
 public class SimpleController : MonoBehaviour
 {
-    public GameObject gameObject;
+    public float speed = 1.0f;
+    public float angularSpeed = 1.5f;
+
+    private Rigidbody rb;
+    private float xMove;
+    private float zMove;
+    private Vector3 forwardDirection;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))  
-        {  
-            gameObject.transform.Translate(2f * Vector3.forward * Time.deltaTime);  
-        }  
-         
-        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))  
-        {  
-            gameObject.transform.Translate(2f * Vector3.back * Time.deltaTime);  
-        }  
-         
-        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
-        {  
-            gameObject.transform.Rotate(Vector3.up, -0.5f);  
-        }  
-        
-        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)) 
-        {  
-            gameObject.transform.Rotate(Vector3.up, 0.5f);  
-        }  
+        // Get key input
+        xMove = Input.GetAxisRaw("Horizontal");
+
+        zMove = Input.GetAxisRaw("Vertical");
+        forwardDirection = transform.forward * zMove;
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = speed * forwardDirection.normalized;
+        rb.angularVelocity = xMove * angularSpeed * Vector3.up;
     }
 }
