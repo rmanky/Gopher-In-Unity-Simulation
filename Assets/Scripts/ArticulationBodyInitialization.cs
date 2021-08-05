3ede806a3bd1afd8ea1b79d563d007bf74ec5848
ZhuoyunZhong <zzhong3@wpi.edu>
2021-08-05 16:59:30 -0400

    add robot prefab

--- Assets/Scripts/ArticulationBodyInitialization.cs
+++ Assets/Scripts/ArticulationBodyInitialization.cs
@@ -3,28 +3,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+///     This script initializes the articulation bodies by 
+///     setting stiffness, damping and force limit of
+///     the non-fixed ones.
+/// </summary>
 public class ArticulationBodyInitialization : MonoBehaviour
 {
     private ArticulationBody[] articulationChain;
 
-    public GameObject robot;
+    public GameObject robotRoot;
+    public bool assignToAllChildren = true;
+    public int robotChainLength = 0;
     public float stiffness = 10000f;
     public float damping = 100f;
     public float forceLimit = 1000f;
 
     void Start()
     {
-        articulationChain = robot.GetComponentsInChildren<ArticulationBody>();
+        // get non-fixed joints
+        articulationChain = robotRoot.GetComponentsInChildren<ArticulationBody>();
         articulationChain = articulationChain.Where(joint => joint.jointType 
                                                     != ArticulationJointType.FixedJoint).ToArray();
-
-        int defDyanmicVal = 100;
         foreach (ArticulationBody joint in articulationChain)
         {
+            Debug.Log(joint.name);
+        }
+
+        // joint length to assign
+        int assignLength = articulationChain.Length;
+        if (!assignToAllChildren)
+            assignLength = robotChainLength;
+
+        // setting stiffness, damping and force limit
+        int defDyanmicVal = 100;
+        for (int i = 0; i < assignLength; i++)
+        {
+            ArticulationBody joint = articulationChain[i];
+
             joint.jointFriction = defDyanmicVal;
             joint.angularDamping = defDyanmicVal;
 
-            ArticulationDrive currentDrive = joint.xDrive;
+            ArticulationDrive currentDrive = joint.yDrive;
             currentDrive.stiffness = stiffness;
             currentDrive.damping = damping;
             currentDrive.forceLimit = forceLimit;
